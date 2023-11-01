d8837d59f743c78dc6828f648f82cd1a1bf3ca12
Ayden Franklin <afranklin@unc.edu>
2023-11-01 17:24:54 -0400

    Create backend branch and start API structure (#11)
    * Create backend branch and start API structure
    
    Created equipment directory in api folder
    Created checkout api folder
    
    * Change checkout.py API file before merge

---
+++ backend/api/equipment/checkout.py
@@ -0,0 +1,7 @@
+"""Equipment Checkout API
+
+This API is used to manage and list user equipment checkouts"""
+
+__authors__ = ["Nicholas Mountain", "Jacob Brown", "Ayden Franklin", "David Sprague"]
+__copyright__ = "Copyright 2023"
+__license__ = "MIT"
